226fb7df47dda75fa20ff1c58bc8ddbdf4d29092
Gulsah Coskun <gulsah.coskun@betbull.com>
2019-10-27 15:18:27 +0300

    first commit

---
+++ src/main/java/com/sbp/processmetrics/ProcessMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.sbp.processmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbp/processmetrics/ProcessMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbp.processmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
